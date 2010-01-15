cc97b5ce38666f35a4c42a1a9382c44e38ddb332
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-01-15 13:40:18 +0000

    * Tagged beta3 @823
    git-svn-id: https://code.todoyu.com/svn/todoyu@826 7f067039-fbb2-4aac-a504-d491c780d6a3

---
+++ tags/todoyu_beta-3/install/assets/js/TodoyuInstaller.js
@@ -0,0 +1,82 @@
+TodoyuInstaller = {
+	
+	/**
+	 * User has agreed to license agreement: hide agreement and show installation dialog
+	 */
+	agreedToLicense: function() {
+		$('licenseagreement').hide();
+		$('installation-steps').show();	
+		$('submit').show();	
+	},
+
+
+
+	/**
+	 * Disable given text box if selected value == 0
+	 * 
+	 * @param	Element	selector 
+	 */
+	disableTextBox: function(selector)	{
+		textbox = document.getElementById('database_new');
+	
+		if(selector.options[selector.selectedIndex].value == '0')	{
+			textbox.disabled = false;
+		} else {
+			textbox.disabled = true;
+		}
+	},
+
+
+
+	/**
+	 * Toggle display of SQL review
+	 * 
+	 * @param	String	togglerID
+	 * @param	String	sqlElementID	 
+	 */
+	toggle: function(togglerID, sqlElementID) {
+		var toggler	= $(togglerID);
+
+		if ( ! toggler.hasClassName('expanded') ) {
+				// Collapse toggler
+			toggler.innerHTML	= 'Hide SQL';
+			toggler.addClassName('expanded');
+			$(sqlElementID).show();						 			
+		} else {
+				// Expand toggler
+			toggler.innerHTML	= 'View SQL';
+			toggler.removeClassName('expanded');
+			$(sqlElementID).hide();
+		}
+	},
+
+
+
+	/**
+	 * Ensure password and it's repetition are identical
+	 */
+	validatePasswordRepetition: function() {
+		var areIdentic	= ( $F('password') == $F('password_confirm') );
+		var longEnough	= $F('password').length >= 5;
+
+		if ( areIdentic && longEnough ) {
+			$('passwordLabel').removeClassName('redLabel');
+			$('passwordConfirmLabel').removeClassName('redLabel');
+			$('submit').show();
+		} else {
+			$('passwordLabel').addClassName('redLabel');
+			$('passwordConfirmLabel').addClassName('redLabel');
+			$('submit').hide();
+		}
+	},
+
+
+
+	/**
+	 *	Skip data import action (by altering action to next one) 
+	 */
+	skipDataImport: function() {
+		document.getElementById('action').value = 'config';
+	}
+
+};
---
+++ tags/todoyu_beta-3/install/config/init.php
@@ -0,0 +1,42 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 snowflake productions gmbh
+*  All rights reserved
+*
+*  This script is part of the todoyu project.
+*  The todoyu project is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License, version 2,
+*  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
+*  the Free Software Foundation;
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Init todoyu installer
+ *
+ * @package		Todoyu
+ * @subpackage	Installer
+ */
+
+	// Activate error reporting
+error_reporting(E_ALL ^ E_NOTICE);
+
+	// Declare PATH constants
+require_once( dirname(__FILE__) . '/../../core/config/constants.php');
+
+	// Check if _ENABLE file is available (installer has finished). Redirect to login
+if( ! is_file(PATH . '/install/ENABLE') ) {
+	@unlink(PATH . '/index.html');
+	header('Location: ../index.php');
+	exit();
+}
+
+?>
