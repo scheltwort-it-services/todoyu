439d60ec30ab17eabd9f0195b88cdafff7b3a4ad
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-01-17 16:51:53 +0000

    * updated copyright year (2010 => 2011) of core tag 2.0.7
    git-svn-id: https://code.todoyu.com/svn/todoyu@4380 7f067039-fbb2-4aac-a504-d491c780d6a3

--- tags/todoyu_2-0-7/core/assets/js/OverflowWindow.js
+++ tags/todoyu_2-0-7/core/assets/js/OverflowWindow.js
@@ -1,10 +1,21 @@
-/**
- * Created by IntelliJ IDEA.
- * User: ferni
- * Date: 08.09.2010
- * Time: 16:40:24
- * To change this template use File | Settings | File Templates.
- */
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2011, snowflake productions GmbH, Switzerland
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSD License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
 
 Todoyu.OverflowWindows = {};
 
