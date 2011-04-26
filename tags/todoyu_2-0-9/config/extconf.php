658b16a2911b2766c389f81c4ac4aae3e8da588b
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-04-26 09:35:01 +0000

    * Tagged todoyu 2.0.9 @5481
    git-svn-id: https://code.todoyu.com/svn/todoyu@5483 7f067039-fbb2-4aac-a504-d491c780d6a3

---
+++ tags/todoyu_2-0-9/config/extconf.php
@@ -0,0 +1,46 @@
+<?php
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2011, snowflake productions GmbH, Switzerland
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSD License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
+
+	// ****************************************************************************
+	// ATTENTION - TODOYU MANAGED FILE
+	// THIS FILE IS AUTOMATICALLY UPDATED BY TODOYU
+	//
+	// EVERYTHING EXCEPT THE 'extConf' KEYS WILL BE REMOVED ON NEXT CONFIG UPDATE
+	// ****************************************************************************
+
+	// Extension config
+Todoyu::$CONFIG['EXT']['admin']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['project']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['sysmanager']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['loginpage']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['portal']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['calendar']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['timetracking']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['daytracks']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['search']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['bookmark']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['contact']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['comment']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['profile']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['assets']['extConf'] = unserialize(stripslashes('a:0:{}'));
+Todoyu::$CONFIG['EXT']['currency']['extConf'] = unserialize(stripslashes('a:1:{s:11:\"keycurrency\";s:2:\"31\";}'));
+Todoyu::$CONFIG['EXT']['projectbilling']['extConf'] = unserialize(stripslashes('a:4:{s:22:\"days_timelimit_payment\";s:2:\"20\";s:3:\"vat\";s:1:\"8\";s:43:\"remove_manual_project_status_cleared_option\";i:1;s:40:\"remove_manual_task_status_cleared_option\";i:1;}'));
+
+?>
---
+++ tags/todoyu_2-0-9/core/TodoyuErrorHandler.class.php
@@ -0,0 +1,172 @@
+<?php
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2011, snowflake productions GmbH, Switzerland
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSD License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
+
+/**
+ * Global error handler
+ *
+ * @package		Todoyu
+ * @subpackage	Core
+ */
+class TodoyuErrorHandler {
+
+	/**
+	 * Flag. If true, the default php error handler will be called after the custom one
+	 *
+	 * @var	Boolean
+	 */
+	private static $ignoreErros = array(E_NOTICE, E_STRICT);
+
+
+	/**
+	 * Only handle error when true
+	 * Useful to suppress warnings. Be careful with this!
+	 */
+	private static $active	= true;
+
+
+
+	/**
+	 * Handler for TodoyuDbException. Print well formatted error information
+	 * in the current output format
+	 *
+	 * @param	TodoyuDbException		$exception
+	 */
+	public static function handleTodoyuDbException(TodoyuDbException $exception) {
+		if( Todoyu::$CONFIG['DEBUG'] ) {
+			ob_end_clean();
+
+				// Send HTTP error header
+			TodoyuHeader::sendHTTPErrorHeader();
+
+				// Send own error header
+			self::sendPhpErrorHeader('Database error: ' . $exception->getMessage());
+
+			$type = TodoyuHeader::getType();
+
+			if( TodoyuRequest::isAjaxRequest() || $type === 'TEXT' ) {
+				echo $exception->getErrorAsPlain();
+			} elseif( $type === 'JSON' ) {
+				echo $exception->getErrorAsJson();
+			} else {
+				echo $exception->getErrorAsHtml(true);
+			}
+
+			exit();
+		} else {
+			self::endScriptClean('Database error!');
+		}
+	}
+
+
+
+	/**
+	 * Handle normal php errors. Disabled at the moment!
+	 *
+	 * @todo	Decide which errors are reported to the log
+	 * @param	Integer		$errorno
+	 * @param	String		$errorstr
+	 * @param	String		$file
+	 * @param	Integer		$line
+	 * @param	Array		$context
+	 * @return	Boolean
+	 */
+	public static function handleError($errorno, $errorstr, $file, $line, $context) {
+		if( self::$active !== true ) {
+			return true;
+		}
+
+			// If not a notice, log it
+		if( ! in_array($errorno, self::$ignoreErros) ) {
+			Todoyu::log('PHP ERROR: [' . $errorno . '] ' . $errorstr, TodoyuLogger::LEVEL_ERROR);
+				// Send HTTP error header
+			TodoyuHeader::sendHTTPErrorHeader();
+
+			self::sendPhpErrorHeader($errorstr);
+		}
+
+			// If debugging, call normal error handler to display the error
+		if( Todoyu::$CONFIG['DEBUG'] ) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+
+
+	/**
+	 * Clean up and die
+	 *
+	 * @param	String	$message
+	 */
+	public static function endScriptClean($message) {
+		ob_clean();
+
+			// Send HTTP error header
+		TodoyuHeader::sendHTTPErrorHeader();
+
+		TodoyuHeader::sendTypeText();
+		die('ERROR: ' . $message);
+	}
+
+
+
+	/**
+	 * Render simple error message
+	 *
+	 * @param	$title			Error title
+	 * @param	$message		Error message
+	 * @return	String
+	 */
+	public static function renderError($title, $message) {
+		$tmpl	= 'core/view/error.tmpl';
+		$data	= array(
+			'title'		=> $title,
+			'message'	=> $message
+		);
+
+		return render($tmpl, $data);
+	}
+
+
+
+	/**
+	 * Send a php error header
+	 *
+	 * @param	String		$errorMessage
+	 */
+	public static function sendPhpErrorHeader($errorMessage) {
+		TodoyuHeader::sendTodoyuHeader('Php-Error', $errorMessage);
+	}
+
+
+
+	/**
+	 * Enable/disable error handling. If disabled, all errors are ignored
+	 *
+	 * @param	Boolean		$active
+	 */
+	public static function setActive($active = true) {
+		self::$active = $active === true;
+	}
+
+}
+
+?>
