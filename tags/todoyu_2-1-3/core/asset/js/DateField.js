bca31fdb7ab31a7a54f908312bb2837bc7e779a4
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-11-17 16:23:02 +0000

    * Tag todoyu 2.1.3
    git-svn-id: https://code.todoyu.com/svn/todoyu@7857 7f067039-fbb2-4aac-a504-d491c780d6a3

---
+++ tags/todoyu_2-1-3/core/asset/js/DateField.js
@@ -0,0 +1,184 @@
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2011, snowflake productions GmbH, Switzerland
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSD License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
+
+/**
+ * @module	Core
+ */
+
+/**
+ * Helper functions for date fields
+ * Requires the jsCalendar functions Date.parseDate and Date.print
+ *
+ * @class		DateField
+ * @namespace	Todoyu
+ */
+Todoyu.DateField = {
+
+	/**
+	 * Install date field onchange observation for input validation
+	 *
+	 * @method	addValidator
+	 * @param	{String}	idField
+	 * @param	{String}	format
+	 */
+	addValidator: function(idField, format) {
+		$(idField).on('change', 'input', this.validateDateFormat.bind(this, format));
+	},
+
+
+
+	/**
+	 * Check date format on value change
+	 *
+	 * @method	validateDateFormat
+	 * @param	{String}	format
+	 * @param	{Event}		event
+	 * @param	{Element}	input
+	 */
+	validateDateFormat: function(format, event, input) {
+		var dateValue	= $F(input).strip();
+
+			// Remove all errors
+		this.setError(input, false);
+		Todoyu.Notification.closeTypeNotes('date.formaterror');
+
+			// Empty is valid too
+		if( dateValue.empty() ) {
+			return;
+		}
+
+			// Is date in valid format?
+		if( ! Todoyu.Time.isDateString(dateValue, format) ) {
+			Todoyu.notifyError('[LLL:core.date.warning.dateformat.invalid]', 'date.formaterror');
+
+			this.setError(input, true);
+		}
+	},
+
+
+
+	/**
+	 * Mark field as error/valid
+	 *
+	 * @param	{String}	input
+	 * @param	{Boolean}	hasError
+	 */
+	setError: function(input, hasError) {
+		var method	= hasError ? 'addClassName' : 'removeClassName';
+		var field	= $(input).up('.fElement');
+
+		field[method]('error');
+		field.down('.fLabel')[method]('error');
+
+			// Clear error message
+		if( ! hasError ) {
+			var errorMsg = field.down('.errorMessage');
+			if( errorMsg ) {
+				errorMsg.update('');
+			}
+		}
+	},
+
+
+
+	/**
+	 * Get format for the date field with a JsCalendar config
+	 *
+	 * @method	getFormat
+	 * @param	{String|Element}	field		ID or Element
+	 * @return	{String}			Format string
+	 */
+	getFormat: function(field) {
+		return Todoyu.JsCalFormat[$(field).id];
+	},
+
+
+
+	/**
+	 * Get date object from a field with a date in string based on the internal format
+	 *
+	 * @method	getDate
+	 * @param	{String|Element}	field
+	 * @return	{Date}
+	 */
+	getDate: function(field) {
+		return Date.parseDate($F(field), this.getFormat(field));
+	},
+
+
+
+	/**
+	 * Set formatted time string in a dateTime field from given values
+	 *
+	 * @method	setTime
+	 * @param	{String|Element}	field
+	 * @param	{Number}			hour
+	 * @param	{Number}			minute
+	 */
+	setTime: function(field, hour, minute) {
+		var date	= this.getDate(field);
+
+		date.setHours(hour);
+		date.setMinutes(minute);
+
+		$(field).value = date.print(this.getFormat(field));
+	},
+
+
+
+	/**
+	 * Set input field value to formatted string of given date values
+	 *
+	 * @method	setDate
+	 * @param	{String|Element}	field
+	 * @param	{Number}			year
+	 * @param	{Number}			month
+	 * @param	{Number}			day
+	 */
+	setDate: function(field, year, month, day) {
+		var date	= this.getDate(field);
+
+		date.setFullYear(year);
+		date.setMonth(month);
+		date.setDate(day);
+
+		$(field).value = date.print(this.getFormat(field));
+	},
+
+
+
+	/**
+	 * Set input field value to formatted string of given date time values
+	 *
+	 * @method	setDateTime
+	 * @param	{String|Element}	field
+	 * @param	{Number}			year
+	 * @param	{Number}			month
+	 * @param	{Number}			day
+	 * @param	{Number}			hour
+	 * @param	{Number}			minute
+	 * @param	{Number}			second
+	 */
+	setDateTime: function(field, year, month, day, hour, minute, second) {
+		var date	= new Date(year, month, day, hour, minute, second);
+
+		$(field).value = date.print(this.getFormat(field));
+	}
+
+};
