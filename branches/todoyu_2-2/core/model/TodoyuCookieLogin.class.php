d563ed2eb7d198bb2999bbea36a1bf035c8772c4
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-04-25 11:17:18 +0000

    * Added more log infos
    git-svn-id: https://code.todoyu.com/svn/todoyu@10074 7f067039-fbb2-4aac-a504-d491c780d6a3

--- branches/todoyu_2-2/core/model/TodoyuCookieLogin.class.php
+++ branches/todoyu_2-2/core/model/TodoyuCookieLogin.class.php
@@ -68,6 +68,7 @@ class TodoyuCookieLogin {
 					} else {
 						TodoyuLogger::logNotice('Cookie login failed for user [' . $cookieData['username'] . '] (useragent is different than in the encrypted login cookie)', TodoyuLogger::LEVEL_SECURITY);
 						TodoyuLogger::logDebug('Current user agent', '<' . $_SERVER['HTTP_USER_AGENT'] . '>');
+						TodoyuLogger::logDebug('User Agent Hash Compare: expect: <' . $cookieData['useragentHash'] . '>, actual <' . $userAgendHash . '>');
 					}
 				} else {
 					TodoyuLogger::logNotice('Decrypted cookie date is not an array', TodoyuLogger::LEVEL_ERROR, $cookieData);
