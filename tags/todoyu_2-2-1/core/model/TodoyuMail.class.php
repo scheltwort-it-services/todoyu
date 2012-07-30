6b1a9ada88025f87ee54e1db5215ca2c59a3e67f
nkarrer <nkarrer@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-07-30 12:07:04 +0000

    * tagged version 2-2-1
    git-svn-id: https://code.todoyu.com/svn/todoyu@11233 7f067039-fbb2-4aac-a504-d491c780d6a3

---
+++ tags/todoyu_2-2-1/core/model/TodoyuMail.class.php
@@ -0,0 +1,362 @@
+<?php
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2012, snowflake productions GmbH, Switzerland
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSD License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
+
+require_once( PATH_LIB . '/php/phpmailer/class.phpmailer-lite.php' );
+
+/**
+ * Todoyu mail
+ *
+ * @package		Todoyu
+ * @subpackage	Core
+ */
+class TodoyuMail extends PHPMailerLite {
+
+	/**
+	 * Temporary HTML content. Render it before sending
+	 *
+	 * @var	String
+	 */
+	private $contentHTML = '';
+
+	/**
+	 * Additional CSS styles
+	 *
+	 * @var	Array
+	 */
+	private $cssStyles	= array();
+
+	/**
+	 * Headline of the todoyu email
+	 *
+	 * @var	String
+	 */
+	private $headline	= null;
+
+	/**
+	 * Default config
+	 *
+	 * @var	Array
+	 */
+	private $config = array(
+		'exceptions'=> true,
+		'mailer'	=> 'mail',
+		'charset'	=> 'utf-8'
+	);
+
+
+
+	/**
+	 * Initialize with config
+	 *
+	 * @param	Array		$config
+	 */
+	public function __construct(array $config = array()) {
+		$config	= TodoyuHookManager::callHookDataModifier('core', 'mail.construct', $config);
+
+		$this->config	= TodoyuArray::mergeRecursive($this->config, $config);
+
+		parent::__construct($this->config['exceptions']);
+
+			// Config
+		$this->Mailer	= $this->config['mailer'];
+		$this->CharSet	= $this->config['charset'];
+
+		if( is_array($this->config['from']) ) {
+			$this->SetFrom($this->config['from']['email'], $this->config['from']['name'], 0);
+		} elseif( is_numeric($this->config['from']) ) {
+			$this->setSender($this->config['from']);
+		} elseif( $this->config['from'] !== false ) {
+			$this->setSystemAsSender();
+		}
+	}
+
+
+
+	/**
+	 * Render and set email HTML message
+	 *
+	 * @return void
+	 */
+	private function renderHtmlContent() {
+		$tmpl	= 'core/view/email-html.tmpl';
+		$data	= array(
+			'content'	=> $this->fullyQualifyLinksInHtml($this->contentHTML),
+			'subject'	=> $this->Subject,
+			'headline'	=> $this->headline,
+			'cssStyles'	=> $this->cssStyles
+		);
+
+		$html	= Todoyu::render($tmpl, $data);
+
+		$this->MsgHTML($html, PATH);
+	}
+
+
+
+
+	/**
+	 * Prefix links with TODOYU_URL to make them work in mails
+	 *
+	 * @param	String		$html
+	 * @return	String
+	 */
+	private function fullyQualifyLinksInHtml($html) {
+		$pattern	= '/href=["\']{1}([^"\']*?)["\']{1}/is';
+		$replace	= array();
+
+		preg_match_all($pattern, $html, $matches);
+
+		foreach($matches[1] as $link) {
+			if( strncmp('http', $link, 4) === 0 ) {
+				continue;
+			}
+			if( strncmp('javascript', $link, 10) === 0 ) {
+				continue;
+			}
+
+			$replace[$link] = SERVER_URL . '/' . ltrim($link, '/');
+		}
+
+		return str_replace(array_keys($replace), array_values($replace), $html);
+	}
+
+
+
+	/**
+	 * Set email headline
+	 * Can be a label
+	 *
+	 * @param	String		$headline
+	 */
+	public function setHeadline($headline) {
+		$headline	= Todoyu::Label($headline);
+		$headline	= TodoyuHookManager::callHookDataModifier('core', 'mail.setHeadline', $headline);
+
+		$this->headline	= $headline;
+	}
+
+
+
+	/**
+	 * Add CSS style code
+	 *
+	 * @param	String		$cssStyle
+	 */
+	public function addCssStyles($cssStyle) {
+		$this->cssStyles[] = $cssStyle;
+	}
+
+
+
+	/**
+	 * Send mail
+	 *
+	 * @param	Boolean		$catchExceptions		Catch the exceptions and log them automatically. Returns true or false
+	 * @return	Boolean		Sending was successful
+	 */
+	public function send($catchExceptions = true) {
+		$this->renderHtmlContent();
+		$status = false;
+
+		if( $catchExceptions ) {
+			try {
+				$status = parent::Send();
+			} catch(phpmailerException $e) {
+				TodoyuLogger::logError($e->getMessage());
+			} catch(Exception $e) {
+				TodoyuLogger::logError($e->getMessage());
+			}
+		} else {
+			$status = parent::Send();
+		}
+
+		return $status;
+	}
+
+
+
+	/**
+	 * Set system as sender of the email (system name and email)
+	 */
+	public function setSystemAsSender() {
+		$this->SetFrom(Todoyu::$CONFIG['SYSTEM']['email'], Todoyu::$CONFIG['SYSTEM']['name'], 0);
+	}
+
+
+
+	/**
+	 * Set currently logged in user as sender
+	 * Fallback to system if no user is logged in
+	 */
+	public function setCurrentUserAsSender() {
+		$idPerson	= Todoyu::personid();
+
+		if( $idPerson === 0 ) {
+			$this->setSystemAsSender();
+		} else {
+			$this->setSender($idPerson);
+		}
+	}
+
+
+
+
+	/**
+	 * Set mail subject
+	 *
+	 * @param	String		$subject
+	 */
+	public function setSubject($subject) {
+		$subject	= Todoyu::Label($subject);
+
+		$subject	= TodoyuHookManager::callHookDataModifier('core', 'mail.setSubject', $subject);
+
+		$this->Subject = $subject;
+	}
+
+
+
+	/**
+	 * Set html content of the mail
+	 *
+	 * @param	String		$html
+	 */
+	public function setHtmlContent($html) {
+		$this->contentHTML	= $html;
+	}
+
+
+
+	/**
+	 * Set plaintext content of the mail
+	 *
+	 * @param	String		$text
+	 */
+	public function setTextContent($text) {
+		$this->AltBody = $text;
+	}
+
+
+
+
+	/**
+	 * Add an attachment
+	 *
+	 * @param	String		$path
+	 * @param	String		$name
+	 */
+	public function AddAttachment($path, $name) {
+		$path	= TodoyuFileManager::pathAbsolute($path);
+
+		parent::AddAttachment($path, $name);
+	}
+
+
+
+	/**
+	 * Add a person as receiver
+	 *
+	 * @param	Integer		$idPerson
+	 * @return	Boolean
+	 */
+	public function addReceiver($idPerson) {
+		$idPerson	= (int) $idPerson;
+		$person		= TodoyuContactPersonManager::getPerson($idPerson);
+
+		$email		= $person->getEmail();
+		$fullname	= $person->getFullName();
+
+		$hookParams	= array(
+			'idPerson'		=> $idPerson,
+			'TodoyuMail'	=> $this
+		);
+		$email		= TodoyuHookManager::callHookDataModifier('core', 'mail.addReceiver.email', $email, $hookParams);
+
+		if( !$email ) {
+			return false;
+		}
+
+		$fullname	= TodoyuHookManager::callHookDataModifier('core', 'mail.addReceiver.fullname', $fullname, $hookParams);
+
+		$this->AddAddress($email, $fullname);
+
+		return true;
+	}
+
+
+
+	/**
+	 * Add a person as reply to
+	 *
+	 * @param	Integer		$idPerson
+	 * @return	Boolean
+	 */
+	public function addReplyToPerson($idPerson) {
+		$idPerson	= (int) $idPerson;
+		$person		= TodoyuContactPersonManager::getPerson($idPerson);
+
+		$email		= $person->getEmail();
+
+		if( !$email ) {
+			return false;
+		}
+
+		$this->AddReplyTo($email, $person->getFullName());
+
+		return true;
+	}
+
+
+
+	/**
+	 * Add current person as reply to
+	 *
+	 * @return	Boolean
+	 */
+	public function addCurrentPersonAsReplyTo() {
+		return $this->addReplyToPerson(Todoyu::personid());
+	}
+
+
+
+	/**
+	 * Set sender of the email
+	 *
+	 * @param  Integer		$idPerson
+	 * @return Boolean
+	 */
+	public function setSender($idPerson) {
+		$idPerson	= (int) $idPerson;
+		$person		= TodoyuContactPersonManager::getPerson($idPerson);
+
+		$email		= $person->getEmail();
+
+		if( !$email ) {
+			return false;
+		}
+
+		$this->SetFrom($email, $person->getFullName());
+
+		return true;
+	}
+
+}
+
+?>
---
+++ tags/todoyu_2-2-1/core/model/form/TodoyuFormElement_Radio.class.php
@@ -0,0 +1,300 @@
+<?php
+/****************************************************************************
+* todoyu is published under the BSD License:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* Copyright (c) 2012, snowflake productions GmbH, Switzerland
+* All rights reserved.
+*
+* This script is part of the todoyu project.
+* The todoyu project is free software; you can redistribute it and/or modify
+* it under the terms of the BSD License.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD License
+* for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script.
+*****************************************************************************/
+
+/**
+ * class for radio boxes
+ *
+ * @package		Todoyu
+ * @subpackage	Form
+ */
+class TodoyuFormElement_Radio extends TodoyuFormElement {
+
+	/**
+	 * Constructor of the class
+	 *
+	 * @param	String		$name
+	 * @param	TodoyuFormFieldset	$fieldset
+	 * @param	Array		$config
+	 */
+	function __construct($name, TodoyuFormFieldset $fieldset, array $config  = array()) {
+		parent::__construct('radio', $name, $fieldset, $config);
+
+//		if( ! $this->isLazyInit() ) {
+//			$this->init();
+//		}
+	}
+
+
+
+	/**
+	 * Init
+	 */
+	protected function init() {
+		if( is_array($this->config['source']) ) {
+			$type	= $this->config['source']['@attributes']['type'];
+			$source	= $this->config['source'];
+
+			switch( $type ) {
+				case 'list':
+					$this->initSourceList($source);
+					break;
+
+
+				case 'function':
+					$this->initSourceFunction($source);
+					break;
+
+
+				case 'sql':
+					$this->initSourceSql($source);
+					break;
+
+
+				default:
+					die("Unknown source tpye");
+					break;
+			}
+		}
+	}
+
+
+
+	/**
+	 * Init options from a XML list
+	 *
+	 * @param	Array		$source
+	 */
+	protected function initSourceList(array $source) {
+		if( is_array($source['option']) ) {
+			foreach($source['option'] as $option) {
+				$this->addOption($option['value'], $option['label'], $option['checked']);
+			}
+		}
+	}
+
+
+
+	/**
+	 * Load select options from database
+	 *
+	 * @param	Array		$source
+	 * @deprecated
+	 * @todo	Remove
+	 */
+	protected function initSourceSql(array $source) {
+		$data	= Todoyu::db()->getArray(
+			$source['fields'],
+			$source['tables'],
+			$source['where'],
+			$source['group'],
+			$source['order'],
+			$source['limit']
+		);
+
+			// Key for label and value
+		$valueKey	= $source['value'];
+		$labelKey	= $source['label'];
+
+			// Set flag
+		$useValueFunc = false;
+		$useLabelFunc = false;
+
+		if( strstr($valueKey, '::') !== false ) {
+			$valueFunc = explode('::', $valueKey);
+			if( method_exists($valueFunc[0], $valueFunc[1]) ) {
+				$useValueFunc = true;
+			}
+		}
+
+		if( strstr($labelKey, '::') !== false ) {
+			$labelFunc = explode('::', $labelKey);
+			if( method_exists($labelFunc[0], $labelFunc[1]) ) {
+				$useLabelFunc = true;
+			}
+		}
+
+		foreach( $data as $option ) {
+			$value	= $useValueFunc ? call_user_func($valueFunc, $this, $option) : $option[$valueKey];
+			$label	= $useLabelFunc ? call_user_func($labelFunc, $this, $option) : $option[$labelKey];
+
+			$this->addOption($value, $label);
+		}
+	}
+
+
+
+	/**
+	 * Initialize source function
+	 *
+	 * @param	Array	$source
+	 */
+	protected function initSourceFunction(array $source) {
+		if( TodoyuFunction::isFunctionReference($source['function']) ) {
+			$options	= TodoyuFunction::callUserFunction($source['function'], $this->getForm());
+
+			foreach($options as $option) {
+				$this->addOption($option['value'], $option['label'], $option['checked']);
+			}
+		}
+	}
+
+
+
+	/**
+	 * Detect if lazy init is defined (grab data when form is rendered)
+	 *
+	 * @return	Boolean
+	 */
+	protected function isLazyInit() {
+		return isset($this->config['source']['lazyInit']);
+	}
+
+
+
+	/**
+	 * Get all options
+	 *
+	 * @return	Array
+	 */
+	public function getOptions() {
+		return TodoyuArray::assure($this->config['options']);
+	}
+
+
+
+	/**
+	 * Add a new option at the end of the list
+	 *
+	 * @param	String		$value
+	 * @param	String		$label
+	 * @param	Boolean		$checked
+	 * @param	Boolean		$disabled
+	 */
+	public function addOption($value, $label, $checked = false, $disabled = false) {
+		$this->config['options'][] = array(
+			'value'		=> $value,
+			'label'		=> Todoyu::Label($label),
+			'checked'	=> $checked,
+			'disabled'	=> $disabled
+		);
+
+		if( $checked ) {
+			$this->addCheckedValue($value);
+		}
+	}
+
+
+
+	/**
+	 * Set an option. The (first) option with the same value will be replaced.
+	 * If no option with this value exists, a new options will be added
+	 *
+	 * @param	String		$value
+	 * @param	String		$label
+	 * @param	Boolean		$checked
+	 * @param	Boolean		$disabled
+	 */
+	public function setOption($value, $label, $checked = false, $disabled = false) {
+		$index = $this->getOptionIndexByValue($value);
+
+		if( !$index ) {
+			$this->addOption($value, $label, $checked, $disabled);
+		} else {
+			$this->config['options'][$index] =  array(
+				'value'		=> $value,
+				'label'		=> $label,
+				'checked'	=> $checked
+			);
+		}
+	}
+
+
+
+	/**
+	 * Get the index of the option by its value
+	 *
+	 * @param	String		$value
+	 * @return	Integer		Or false if not found
+	 */
+	protected function getOptionIndexByValue($value) {
+		$optionIndex = false;
+
+		foreach( $this->config['options'] as $index => $option ) {
+			if( $option['value'] == $value ) {
+				$optionIndex = $index;
+				break;
+			}
+		}
+
+		return $optionIndex;
+	}
+
+
+
+	/**
+	 * Get selected value
+	 *
+	 * @return	Mixed		An array or a string
+	 */
+	public function getValue() {
+		return $this->config['checked'];
+	}
+
+
+
+	/**
+	 * Set selected value
+	 *
+	 * @param	Mixed		$value		An array or a string
+	 */
+	public function setValue($value) {
+		$this->config['checked'] = $value;
+	}
+
+
+
+	/**
+	 * Add value to selected-values list
+	 *
+	 * @param	String		$value
+	 */
+	public function addCheckedValue($value) {
+		$this->setValue(TodoyuString::addToList($this->getValue(), $value, ',', true));
+	}
+
+
+
+	/**
+	 * Get data for template rendering
+	 *
+	 * @return	Array
+	 */
+	protected function getData() {
+		if( $this->isLazyInit() ) {
+			$this->init();
+		}
+
+		return parent::getData();
+	}
+
+
+}
+
+?>
